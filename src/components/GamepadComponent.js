75aafd01e1c8572e3e561ff48b76b8e2ca21b2f6
roeeyud <roeeyud@gmail.com>
2020-03-23 15:48:30 +0200

    Finished example

--- src/components/GamepadComponent.js
+++ src/components/GamepadComponent.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import Fab from '@material-ui/core/Fab';
+import Gamepad from '@material-ui/icons/Gamepad';
+
+import { Context as ControllerContext } from '../context/Controller';
 
 export default function GamepadComponent() {
+    const { onControllerChange } = useContext(ControllerContext);
 
+    return <Fab aria-label="peripheralController" 
+        onMouseDown={() => onControllerChange('demo-value', 'down')}
+        onMouseUp={() => onControllerChange('demo-value', 'up')}
+    >
+        <Gamepad />
+  </Fab>
 }
