cf2e02517393fcae5fa4c96dccf54e7530a0b387
Lior Rabinovich <lior.px@gmail.com>
2020-03-24 03:04:06 +0200

    test

--- src/App.js
+++ src/App.js
@@ -4,29 +4,8 @@ import Home from './containers/Home'
 import Page from './containers/Page'
 
 export default function App() {
-  useEffect(() => {
-    Notification.requestPermission();
-  }, []);
-
-  function randomNotification() {
-    var randomItem = Math.floor(Math.random());
-    var notifTitle = 'Test';
-    var notifBody = 'Created by Lior';
-    var notifImg = 'https://jpg2pdf.com/images/jpg2pdf/icon.png';
-    var options = {
-      body: notifBody,
-      icon: notifImg
-    }
-    var notif = new Notification(notifTitle, options);
-  }
-
-  const onClick = () => {
-    randomNotification();
-  }
-
   return (
     <Router>
-      <button onClick={onClick}>Notification</button>
       <div className="container">
         <Route path="/" exact component={Page} />
         <Route path="/page" component={Home} />
