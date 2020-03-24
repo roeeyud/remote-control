724cdb72e635ec770be866a6c829eb60299c6882
Lior Rabinovich <lior.px@gmail.com>
2020-03-24 03:03:14 +0200

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import theme from './theme';
 import "./index.css";
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
@@ -15,7 +15,4 @@ ReactDOM.render(
   document.querySelector('#root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
