480ffaf2e478ee4abb9af82ee2ae43a4bffcc4d8
Lior Rabinovich <lior.px@gmail.com>
2020-03-26 22:32:54 +0200

    test

--- src/containers/Page.js
+++ src/containers/Page.js
@@ -10,25 +10,23 @@ const useStyles = makeStyles({
 
 export default function Page() {
   const classes = useStyles();
+  const $video = useRef(null);
 
   useEffect(() => {
-    // Grab elements, create settings, etc.
-    var video = document.getElementById('video');
-
     // Get access to the camera!
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       // Not adding `{ audio: true }` since we only want video now
       navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
         //video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.play();
+        $video.current.srcObject = stream;
+        $video.current.play();
       });
     }
   }, []);
 
   return (
     <div className={classes.root}>
-      <video id="video" width="640" height="480" autoplay></video>
+      <video ref={$video} width="640" height="480" autoplay></video>
       <button id="snap">Snap Photo</button>
       <canvas id="canvas" width="640" height="480"></canvas>
     </div>
