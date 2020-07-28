6fd1ca40f4505845d06a8899af47969d1ec17436
roeeyud <Roeeyud@gmail.com>
2020-07-28 20:17:35 +0300

    Changed map to stright line

--- src/components/Map/MapCanvas.js
+++ src/components/Map/MapCanvas.js
@@ -1,94 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { Graphics, useTick } from "pixi.js";
+import Typography from "@material-ui/core/Typography";
 import { Stage, PixiComponent } from "@inlet/react-pixi";
 
-const mapSize = 200;
-const classSize = 30;
-const robotSize = 25;
+const mapSize = 250;
+const classSize = 50;
+const robotSize = 40;
 const robotLineSize = 3;
-const robotLineDistance = 10;
-const middlePoint = 5;
-const middleSpace = 13;
-const endPoint = mapSize - classSize;
+const startX = classSize / 2;
+const startY = 0;
 
 function getPoints() {
   return [
-    { x: 0, y: endPoint },
-    { x: 50, y: endPoint },
-    { x: 80, y: endPoint },
-    { x: 110, y: endPoint },
-    { x: 140, y: endPoint },
-    { x: endPoint, y: endPoint },
-    { x: endPoint, y: 140 },
-    { x: endPoint, y: 110 },
-    { x: endPoint, y: 80 },
-    { x: endPoint, y: 50 },
-    { x: endPoint, y: 0 },
-  ];
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+  ].map((name, index) =>
+    ({ x: startX, y: startY + (classSize * index), name }));
 }
 
-function getRobotMovePosition(x, y, point) {
-  if (point > middlePoint) {
-    return { x, y: y + robotSize };
-  }
-
+function getRobotMovePosition(x, y) {
   return { x: x + robotSize, y };
-}
-
-function getRobotPointPosition(x, y, point) {
-  if (point === middlePoint) {
-    return { x: endPoint - middleSpace, y };
-  }
-
-  return { x, y };
-}
-
-function calcAngle(number) {
-  // number is number between 1 0
-  if (number > 0.5) {
-    return number * 10;
-  }
-
-  return number * -10;
-}
-
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-function getRobotLinePointPosition({ x, y, point }) {
-  if (point > middlePoint) {
-    return { x, y };
-  }
-
-  return { x: x + robotSize, y };
-}
-
-function getRobotLineMovePosition({ x, y, angle, point }) {
-  if (point === middlePoint) {
-    return { x, y };
-  }
-
-  if (point > middlePoint) {
-    return { x: x - calcAngle(angle), y: y - robotLineDistance * 2 };
-  }
-
-  return { x: x + robotSize + robotLineDistance / 2, y: y + calcAngle(angle) };
 }
 
 const Map = PixiComponent("Map", {
-  create: (props) => new Graphics(),
+  create: () => new Graphics(),
   applyProps: (instance, _, props) => {
     const { points } = props;
     instance.clear();
     instance.lineStyle(classSize, 0xe0e0e0);
     instance.moveTo(points[0].x, points[0].y);
-    points.forEach(({ x, y }) => {
-      instance.lineTo(x, y);
+    points.forEach(({ x, y }, index) => {
+      instance.lineTo(x, y + (classSize));
     });
     instance.endFill();
   },
 });
+
+function getRobotCenter(pointIndex, points) {
+  const offset = (robotSize / 2);
+  const diff = (classSize - robotSize) / 2;
+  const pointX = points[pointIndex].x - offset;
+  const pointY = points[pointIndex].y + offset + diff;
+  
+  return { x: pointX + (robotSize / 2),  y: pointY }
+}
 
 const Robot = PixiComponent("Robot", {
   create: () => new Graphics(),
@@ -96,11 +54,10 @@ const Robot = PixiComponent("Robot", {
     const { robot, points } = props;
     const { point } = robot;
 
-    const { x: pointX, y: pointY } = getRobotPointPosition(
-      points[point].x,
-      points[point].y,
-      point
-    );
+    const offset = (robotSize / 2);
+    const diff = (classSize - robotSize) / 2;
+    const pointX = points[point].x - offset;
+    const pointY = points[point].y + offset + diff;
     const { x: robotX, y: robotY } = getRobotMovePosition(
       pointX,
       pointY,
@@ -120,21 +77,15 @@ const RobotLine = PixiComponent("RobotLine", {
   applyProps: (instance, _, props) => {
     const { robot, points } = props;
     const { point, angle } = robot;
-
-    const { x: pointX, y: pointY } = getRobotPointPosition(
-      points[point].x,
-      points[point].y,
-      point
-    );
-    
-    const lineStartX = pointX + (robotSize / 2) - (robotLineSize / 2);
-    const lineStartY = pointY;
-    const targetX = lineStartX + (robotSize * Math.sin(Math.PI * 2 * angle));
-    const targetY = lineStartY + (robotSize * Math.cos(Math.PI * 2 * angle));
+    const fixedAngle =  ((angle % 1 * 2) > 1) ? 0.5 : 0;
+    const { x: lineStartX, y: lineStartY } = getRobotCenter(point, points);
+    const targetX = lineStartX + (robotSize * Math.sin(Math.PI * 2 * fixedAngle));
+    const targetY = lineStartY + (robotSize * Math.cos(Math.PI * 2 * fixedAngle));
 
     instance.clear();
-    instance.lineStyle(robotLineSize, 0xff0000);
-    instance.moveTo(lineStartX , lineStartY);
+    instance.lineStyle(robotLineSize, 0xcc4444);
+    
+    instance.moveTo(lineStartX, lineStartY);
     instance.lineTo(targetX, targetY);
     instance.endFill();
   },
@@ -146,7 +97,7 @@ export default function MapCanvas() {
   useEffect(() => {
     function fetchLocation() {
       const xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
           setRobotData(JSON.parse(this.response));
         }
@@ -159,13 +110,26 @@ export default function MapCanvas() {
     return () => clearInterval(interval);
   }, [setRobotData]);
 
-  if(!robotData) {
+  if (!robotData) {
     return null;
   }
-  return (
+  return (<div style={{ display: 'flex' }}>
+    <div style={{ marginRight: 20 }}>
+      {getPoints().map(({ name }, index) => (
+        <div style={{ height: classSize, display: 'flex', alignItems: 'center' }}>
+          <Typography variant="body1" style={{
+            color: robotData.point === index ? '#19bf89' : 'white',
+            fontWeight: robotData.point === index ? 'bold' : 'initial',
+          }}>
+            {name}
+          </Typography>
+        </div>
+
+      ))}
+    </div>
     <Stage
-      width={mapSize}
-      height={mapSize}
+      width={classSize}
+      height={classSize * getPoints().length}
       options={{
         transparent: true,
         resolution: window.devicePixelRatio,
@@ -176,5 +140,5 @@ export default function MapCanvas() {
       <Robot robot={robotData} points={points} />
       <RobotLine robot={robotData} points={points} />
     </Stage>
-  );
+  </div>);
 }
